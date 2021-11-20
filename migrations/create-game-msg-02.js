6531d28e10a9149ffb5fdca88b80193ac1b4f74f
J1aM1ng <2605379072@qq.com>
2021-11-20 19:17:49 +0800

    add mysql

--- migrations/create-game-msg-02.js
+++ migrations/create-game-msg-02.js
@@ -1,36 +1,34 @@
 "use strict";
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("GameMsgs", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      userId: {
-        type: Sequelize.INTEGER,
-      },
-      level: {
-        type: Sequelize.INTEGER,
-      },
-      score: {
-        type: Sequelize.INTEGER,
-      },
-      time: {
-        type: Sequelize.INTEGER,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("GameMsgs");
-  },
-};
+export async function up(queryInterface, Sequelize) {
+  await queryInterface.createTable("GameMsgs", {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+    },
+    level: {
+      type: Sequelize.INTEGER,
+    },
+    score: {
+      type: Sequelize.INTEGER,
+    },
+    time: {
+      type: Sequelize.INTEGER,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  });
+}
+export async function down(queryInterface, Sequelize) {
+  await queryInterface.dropTable("GameMsgs");
+}
