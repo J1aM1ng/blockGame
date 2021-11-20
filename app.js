a5bf0ba95dc16c10eb213110f0f4d059d54bff80
J1aM1ng <2605379072@qq.com>
2021-11-20 22:47:23 +0800

    fix: style

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+import createError from "http-errors";
+import express, { json, urlencoded, static } from "express";
+import { join } from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-var indexRouter = require("./server/index");
-var usersRouter = require("./server/users");
-var gameUsersRouter = require("./server/gameUsers");
-var gameMsgsRouter = require("./server/gameMsgs");
+import indexRouter from "./server/index";
+import usersRouter from "./server/users";
+import gameUsersRouter from "./server/gameUsers";
+import gameMsgsRouter from "./server/gameMsgs";
 
-var app = express();
+let app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(static(join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -42,4 +42,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-module.exports = app;
+export default app;
